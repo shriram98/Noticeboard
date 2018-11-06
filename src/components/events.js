e35be546a437a353423d0b552a00023d6da4b268
shriram <shriramvenugopal123@gmail.com>
2018-11-06 17:55:01 +0530

    created basic layout for webapp

--- src/components/events.js
+++ src/components/events.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
 
+import SlideShow from './slideshow.js'
 
 export default class Events extends Component {
     render() {
         return(
-            <div></div>
+            <SlideShow />
         );
     }
 }
--- src/components/header.js
+++ src/components/header.js
@@ -6,15 +6,18 @@ export default class Header extends Component {
     render() {
         return(
             <div className="home-container">
+                <div className="login-btn"><img src="#" /></div>                
                 <nav>
                     <div id="nav-logo">Logo</div>
-                    <div className="navbar">
-                        <div className="navbar-item">Events</div>
-                        <div className="navbar-item">Categories</div>
-                        <div className="navbar-item">Contact</div>
-                    </div>
+                    <div className="nav-item nav-events">Events</div>
+                    <div className="nav-item">Categories</div>
+                    <div className="nav-item">Contact</div>
                 </nav>
-                
+                <div className="home-title--wrapper">
+                    <div className="intro-text typewriter">All the Events at one place</div>
+                    <button className="home-btn btn-login">Login</button><br />
+                    <button className="home-btn btn-next">Show me more</button>
+                </div>
             </div>
         );
     }
