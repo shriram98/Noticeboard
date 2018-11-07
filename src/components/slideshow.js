53e609cbd245437de4757cc06364d55e288b74bb
shriram <shriramvenugopal123@gmail.com>
2018-11-07 20:31:03 +0530

    added basic slideshow layout

--- src/components/slideshow.js
+++ src/components/slideshow.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react'
 import Slider from 'react-slick'
 
+import '../css/slideshow.css'
+import "slick-carousel/slick/slick.css" 
+import "slick-carousel/slick/slick-theme.css"
+
 export default class SlideShow extends Component {
     render() {
         const settings = {
@@ -11,26 +15,23 @@ export default class SlideShow extends Component {
             slidesToScroll: 1
           };
           return (
-            <div>
-              <h2> Single Item</h2>
+            <div className="slideshow-container">
+              <div className="slideshow-title">Popular Upcoming Events &mdash; in India</div>
               <Slider {...settings}>
                 <div>
-                  <h3>1</h3>
+                  <img className="slideshow-img" src={require("../res/event1.jpg") }/>
                 </div>
                 <div>
-                  <h3>2</h3>
+                  <img className="slideshow-img" src={require("../res/event1.jpg") }/>
                 </div>
                 <div>
-                  <h3>3</h3>
+                  <img className="slideshow-img" src={require("../res/event1.jpg") }/>
                 </div>
                 <div>
-                  <h3>4</h3>
+                  <img className="slideshow-img" src={require("../res/event1.jpg") }/>
                 </div>
                 <div>
-                  <h3>5</h3>
-                </div>
-                <div>
-                  <h3>6</h3>
+                  <img className="slideshow-img" src={require("../res/event1.jpg") }/>
                 </div>
               </Slider>
             </div>
