fe1087d108d9e59b2be31d4c068b7a4aa8f57f54
slaybhi <a.abhijith1adoor@gmail.com>
2018-11-20 16:06:24 +0530

    added the event component

--- src/app.js
+++ src/app.js
@@ -1,32 +1,13 @@
 import React, {Component} from 'react'
+import Events from './component/events/events'
+// import './css/app.css'
 
-import './css/app.css'
-
-import Header from './components/header'
-import Events from './components/events'
-import Categories from './components/categories'
-import Works from './components/works'
-import Footer from './components/footer'
 
 export default class App extends Component {
     render() {
         return(
             <div>
-                <section className="section" id="home">
-                    <Header />
-                </section>
-                <section className="section" id="events">
-                    <Events />
-                </section>
-                <section className="section" id="categories">
-                    <Categories />
-                </section>
-                <section className="section" id="works">
-                    <Works />
-                </section>
-                <footer>
-                    <Footer />
-                </footer>
+                <Events/>
             </div>
         );
     }
---
+++ src/component/events/events.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './CSS/events.css';
+import { Element } from 'react-scroll';
+
+
+import Header from './component/header_footer/Header';
+import Featured from './component/featured';
+import VunueNfo from './component/venueNfo';
+import Highlight from  './component/Highlights'
+import Pricing from './component/pricing';
+import Location from './component/location';
+import Footer from './component/header_footer/Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" style={{ height:"1500px",background:'cornflowerblue'}}>
+        <Header/>
+
+        <Element name="featured">
+          <Featured/>
+        </Element>
+
+        <Element name="venuenfo">
+          <VunueNfo/>
+        </Element>
+        
+        <Element name="highlights">
+          <Highlight/>
+        </Element>
+        
+        <Element name="pricing">
+          <Pricing/>
+        </Element>
+
+        <Element name="location">
+          <Location/>
+        </Element>
+        
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
