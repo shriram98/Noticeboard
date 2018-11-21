8d633ae49ee741aefe9f335525577b880dc0a099
shriram <shriramvenugopal123@gmail.com>
2018-11-21 11:35:42 +0530

    scroll animation

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react'
+import { scroller } from 'react-scroll'
+import { Element } from 'react-scroll'
 
 import '../../css/header.css'
 import LoginSection from './loginSection'
 import CardList from '../categories/CardList'
+import UpcomingEvents from '../upcoming_events/upcoming'
+
 
 export default class Header extends Component {
     constructor(props) {
@@ -10,6 +14,16 @@ export default class Header extends Component {
         this.state = { isLoggedIn: false}
         this.login = this.login.bind(this)
         this.resetLogin = this.resetLogin.bind(this)
+        this.scrollToElement = this.scrollToElement.bind(this)
+    }
+
+    scrollToElement(element) {
+        scroller.scrollTo(element,{
+            duration: 1000,
+            delay:100,
+            smooth: true,
+            offset: 0
+        });
     }
 
     login(e) {
@@ -33,22 +47,34 @@ export default class Header extends Component {
 
 
         return(
-            <div className="home-container">
-                {/* <div className="login-btn" onClick={this.login}><img src="#" /></div>                 */}
-                <nav>
-                    <div id="nav-logo"onClick={this.resetLogin}>Logo</div>
-                    <div className="nav-item nav-events">Events</div>
-                    <div className="nav-item">Categories</div>
-                    <div className="nav-item">Contact</div>
-                </nav>
-                <div className="home-title--wrapper">
-                    <div className="intro-text typewriter">All the Events at one place</div>
-                    <button className="home-btn btn-login" onClick={this.resetLogin}>Login</button><br />
-                    <button className="home-btn btn-next">Show me more</button>
+            <div>
+                <Element name="home">
+                <div className="home-container">
+                    {/* <div className="login-btn" onClick={this.login}><img src="#" /></div>                 */}
+                    <nav>
+                        <div id="nav-logo"onClick={this.resetLogin}>Logo</div>
+                        <div className="nav-item nav-events">Events</div>
+                        <div className="nav-item" onClick={ () => this.scrollToElement('categories')}>Categories</div>
+                        <div className="nav-item">Contact</div>
+                    </nav>
+                    <div className="home-title--wrapper">
+                        <div className="intro-text typewriter">All the Events at one place</div>
+                        <button className="home-btn btn-login" onClick={this.resetLogin}>Login</button><br />
+                        <button className="home-btn btn-next" onClick={ () => this.scrollToElement('upcoming')}>Show me more</button>
+                    </div>
+                    {loginSection}
                 </div>
-                <CardList />
-                {loginSection}
+                </Element>
+                <Element name="upcoming">
+                    <UpcomingEvents />
+                </Element>
+                <Element name="categories">
+                    <div className="cat-container">
+                        <CardList />
+                    </div>
+                </Element>
             </div>
+            
         );
     }
 }
---
+++ src/component/upcoming_events/upcoming.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react' 
+
+import '../../css/upcoming.css'
+
+export default class UpcomingEvents extends Component {
+    render() {
+        return(
+            <div className="up-container">
+                Upcoming Events will be displayed here
+            </div>
+        )
+    }
+}
