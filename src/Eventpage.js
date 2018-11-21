117a3e3faa53d4b413af8bc4f7c9cc0d9d8b4400
slaybhi <a.abhijith1adoor@gmail.com>
2018-11-21 12:18:36 +0530

    added more events with different type

--- src/Eventpage.js
+++ src/Eventpage.js
@@ -6,7 +6,7 @@ var eventlist = [
     {
         speaker: 'Ariana Grande',
         eventname: 'Venue',
-        eventtype: 'Musical',
+        eventtype: 'Music',
         date: 'Dec, 19, 2018',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         contact: ['abc@xyz.com','9496311967'],
@@ -18,7 +18,40 @@ var eventlist = [
     {
         speaker: 'Ariana Grande',
         eventname: 'The Venue',
-        eventtype: 'Musical Event',
+        eventtype: 'Standup',
+        date: 'Dec, 16, 2018',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        contact: ['abc@xyz.com','9496311967'],
+        locationMap: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22676.573915000565!2d72.91426295725398!3d19.136254571742047!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c7f189efc039%3A0x68fdcea4c5c5894e!2sIndian+Institute+of+Technology+Bombay!5e0!3m2!1sen!2sin!4v1542774715016',
+        location: 'IIT Mumbai'
+
+    },
+    {
+        speaker: 'Ariana Grande',
+        eventname: 'The Venue',
+        eventtype: 'Tech',
+        date: 'Dec, 16, 2018',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        contact: ['abc@xyz.com','9496311967'],
+        locationMap: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22676.573915000565!2d72.91426295725398!3d19.136254571742047!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c7f189efc039%3A0x68fdcea4c5c5894e!2sIndian+Institute+of+Technology+Bombay!5e0!3m2!1sen!2sin!4v1542774715016',
+        location: 'IIT Mumbai'
+
+    },
+    {
+        speaker: 'Ariana Grande',
+        eventname: 'The Venue',
+        eventtype: 'Gaming',
+        date: 'Dec, 16, 2018',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        contact: ['abc@xyz.com','9496311967'],
+        locationMap: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22676.573915000565!2d72.91426295725398!3d19.136254571742047!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c7f189efc039%3A0x68fdcea4c5c5894e!2sIndian+Institute+of+Technology+Bombay!5e0!3m2!1sen!2sin!4v1542774715016',
+        location: 'IIT Mumbai'
+
+    },
+    {
+        speaker: 'Ariana Grande',
+        eventname: 'The Venue',
+        eventtype: 'Art',
         date: 'Dec, 16, 2018',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         contact: ['abc@xyz.com','9496311967'],
